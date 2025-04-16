2f47fcad63c5ad4e75f20ff4734ed272ea687b21
EdwinJMataH <ejmh.developer@gmail.com>
2025-04-15 23:59:10 -0600

    feat: add router settings

---
+++ app/core/Router.php
@@ -0,0 +1,66 @@
+<?php
+class Router {
+
+    public array $routesGET  = [];
+    public array $routesPOST = [];
+
+    public function get(string $uri, string $callback) : void {
+        $this->routesGET[$uri] = $callback;
+    }
+
+    public function post(string $uri, string $callback) : void {
+        $this->routesPOST[$uri] = $callback;
+    }
+
+    public function match() : void {
+        $uri      = $_SERVER['PATH_INFO'] ?? '/';
+        $method   = $_SERVER['REQUEST_METHOD'];
+        $routes   = "routes$method";
+        $params   = [];
+        $callback = null;
+        $pattern  = null;
+
+        foreach ($this->$routes as $route => $callback) {
+            //Replace dynamic routes with a pattern
+            if (strpos($route, ':' ) !== false) {
+                $pattern = preg_replace('#:[a-zA-Z]+#','([a-zA-Z]+)', $route);
+            }
+            
+            // If a match is found with the pattern, the parameters and path with the matching pattern are obtained.
+            if (preg_match("#^$pattern$#", $uri, $matches)) {
+                $uri    = $route;
+                $params = array_slice($matches, 1);
+            }
+        }
+
+        if ($method == 'GET') {
+            $callback = $this->routesGET[$uri] ?? null;
+        }
+        if ($method == 'POST') {
+            $callback = $this->routesGET[$uri] ?? null;
+        }
+
+        $data = [...$params];
+        if ($callback) {
+            call_user_func($callback, $data);
+        } else {
+            http_response_code(404);
+            echo "404 Not Found...";
+            return;
+        }
+    }
+
+    public function render($view, $data = []) {
+
+        //Transform array in variables
+        foreach ($data as $key => $value) {
+            $$key = $value;
+        }
+
+        ob_start();
+        require __DIR__ ."/views/$view.php";
+        $content = ob_get_clean();
+        require __DIR__ ."/views/layouts/main.php";
+    }
+
+}
